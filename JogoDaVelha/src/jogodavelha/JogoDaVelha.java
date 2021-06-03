c2098d2502e152a69e7e2c9e3fc3a90e1431145e
gelso <gelso@DESKTOP-LO80210>
2021-06-02 22:45:52 -0300

    criado as classes main e tabuleiro ja exibindo o tabuleiro e aceitando jogadas

---
+++ JogoDaVelha/src/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,13 @@
+package jogodavelha;
+
+public class JogoDaVelha {
+
+
+    public static void main(String[] args) {
+        
+        Tabuleiro.exibe();
+        Tabuleiro.jogada(3,"X");
+        Tabuleiro.exibe();
+    }
+    
+}
---
+++ JogoDaVelha/src/jogodavelha/Tabuleiro.java
@@ -0,0 +1,55 @@
+
+package jogodavelha;
+
+
+public class Tabuleiro {
+    
+ public static String[][] matriz = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+ //public static int[][] matriz = {{1,2,3},{4,5,6},{7,8,9}};
+  //public static String[][] matriz = new String [3][3];
+    
+    
+    public static void exibe(){
+        int x = 0;
+        for(int i=0; i<=2; i++){
+            System.out.println("");
+            for(int z=0; z<=2; z++){
+                System.out.print(matriz[i][z]);
+                x++;
+                if(x <=2){
+                 System.out.print(" | ");   
+                }else{
+                    x=0;
+                }
+                
+            }
+        }
+        System.out.println("\n");
+    }
+    
+    public static void jogada(int jogada, String marcador){
+        int aux=1;
+        int posicao = jogada;
+        int j=0;
+        int k=0;
+      
+        
+        if(aux != posicao){// 
+           aux=0;
+            for(int i=0;((i<=2)&&(aux != posicao));i++){
+                
+                for(int z=0;((z<=2)&&(aux != posicao));z++){
+                    aux++;
+                    k=z;
+                }
+                j=i;
+            }
+        }
+            
+    Tabuleiro.matriz[j][k]= marcador; 
+    }
+    
+}
+        
+
+    
